60fa83a6dba067b88248635c9984701d1122ea6e
Aquaint Development <admin@aquaint.us>
2017-07-31 16:23:51 +0000

    Week 4 weekend changes: trying to integrate user login form into index page.

---
+++ scripts/welcome.js
@@ -0,0 +1,24 @@
+Vue.component('signup',{
+    template:'#signup'
+})
+Vue.component('registration',{
+    template:'#registration'
+})
+var userLogin = new Vue({
+    data:{
+  	current:"signup",
+	reg: false
+    },
+    methods:{
+  	toRegistration:function(){
+    	    this.reg = true,
+    	    this.current = 'registration'
+	},
+	toHome: function(){
+    	    this.current = 'signup',
+	    this.reg = false;
+	},
+    },
+    components:['signup','registration'], 
+})
+userLogin === document.getElementById('aquaint-login')
