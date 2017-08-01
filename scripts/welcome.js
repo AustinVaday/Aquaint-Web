2b289b470d0269f07609e01e42c78691d547e332
Aquaint Development <admin@aquaint.us>
2017-08-01 07:29:52 +0000

    Added very basic vueJS home page

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,24 +1,51 @@
 Vue.component('signup',{
-    template:'#signup'
+	template:'#signup'
 })
 Vue.component('registration',{
-    template:'#registration'
+	template:'#registration'
 })
-var userLogin = new Vue({
-    data:{
+new Vue({
+	el: "#signup-elem",
+	data:{
   	current:"signup",
-	reg: false
-    },
-    methods:{
+    reg: false
+  },
+  methods:{
   	toRegistration:function(){
-    	    this.reg = true,
-    	    this.current = 'registration'
-	},
-	toHome: function(){
-    	    this.current = 'signup',
-	    this.reg = false;
-	},
+    	this.reg = true,
+    	this.current = 'registration'
     },
-    components:['signup','registration'], 
+    toHome: function(){
+    	this.current = 'signup',
+      this.reg = false;
+    },
+  },
+  components:['signup','registration'], 
 })
-userLogin === document.getElementById('aquaint-login')
+
+//Vue.component('signup',{
+//    template:'#signup'
+//})
+//Vue.component('registration',{
+//    template:'#registration'
+//})
+//var userLogin = new Vue({
+//	el: "signup-elem",
+//    data:{
+//	  	current:"signup",
+//		reg: false
+//    },
+//    methods:{
+//  		toRegistration:function(){
+//   	    	this.reg = true,
+//   	    	this.current = 'registration'
+//		},
+//		toHome: function(){
+//   	    	this.current = 'signup',
+//	    	this.reg = false;
+//		},
+//    },
+//    components:['signup','registration'], 
+//})
+
+//userLogin === document.getElementById('aquaint-login')
